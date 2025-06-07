e87df5251cb7a99d280222971c1f46e58ab398db
saylesss88 <saylesss87@proton.me>
2025-06-07 11:19:35 -0400

    terms

--- home/terms/default.nix
+++ home/terms/default.nix
@@ -1,10 +1,12 @@
-# ~/flake/home/terms/default.nix (if only ghostty.nix is imported here)
-{
-  config,
-  lib,
-  ...
-}: {
-  imports =
-    lib.optional config.custom.ghostty.enable ./ghostty.nix
-    ++ lib.optional config.custom.kitty.enable ./kitty.nix;
+{...}: {
+  imports = [
+    ./kitty.nix
+    ./ghostty.nix
+  ];
 }
+# {
+#   imports =
+#     lib.optional config.custom.ghostty.enable ./ghostty.nix
+#     ++ lib.optional config.custom.kitty.enable ./kitty.nix;
+# }
+
