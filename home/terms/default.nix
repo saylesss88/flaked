bc62e446ea8a6c25d5e35aaa50eab29e049002a5
saylesss88 <saylesss87@proton.me>
2025-06-09 18:42:57 -0400

    home default.nix

--- home/terms/default.nix
+++ home/terms/default.nix
@@ -1,10 +1,6 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  imports = builtins.concatLists [
-    (lib.optional config.ghostty ./ghostty.nix)
-    (lib.optional config.kitty ./kitty.nix)
+{...}: {
+  imports = [
+    ./ghostty.nix
+    ./kitty.nix
   ];
 }
