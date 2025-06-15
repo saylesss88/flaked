f96388e8b3a3b1208667d8cdb4a2591e30b263a9
saylesss88 <saylesss87@proton.me>
2025-06-15 14:14:09 -0400

    plymouth

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -43,8 +43,29 @@ in {
       };
       plymouth = {
         enable = true;
-        theme = "spinner";
+        theme = "rings";
+        themePackages = with pkgs; [
+          (adi1090x-plymouth-themes.override {
+            selected_themes = ["rings"];
+          })
+        ];
       };
+      # Enable "Silent Boot"
+      # consoleLogLevel = 0;
+      # initrd.verbose = false;
+      # kernelParams = [
+      #   "quiet"
+      #   "splash"
+      #   "boot.shell_on_fail"
+      #   "loglevel=3"
+      #   "rd.systemd.show_status=false"
+      #   "rd.udev.log_level=3"
+      #   "udev.log_priority=3"
+      # ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      # loader.timeout = 0;
     };
     environment.systemPackages = with pkgs; [greetd.tuigreet];
   };
