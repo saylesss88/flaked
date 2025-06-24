2db3082b4203434379bb47a8330b06325c459645
saylesss88 <saylesss87@proton.me>
2025-06-24 14:48:31 -0400

    sops

--- hosts/magic/sops.nix
+++ hosts/magic/sops.nix
@@ -10,6 +10,7 @@
         owner = "root";
         group = "root";
         mode = "0400";
+        neededForUsers = true;
       };
       "github_deploy_key_ed25519_nix-book" = {
         sopsFile = ../../secrets/github-deploy-key.yaml;
