1d04bfda81f95590f14ff92dc5e2e0dffddd6197
saylesss88 <saylesss87@proton.me>
2025-06-25 11:09:59 -0400

    sops

--- hosts/magic/users.nix
+++ hosts/magic/users.nix
@@ -20,8 +20,7 @@
         # Change me! generate with `mkpasswd -m SHA-512 -s`
         # initialHashedPassword = "$6$knQSQp4le3uiy..3$gAUAugTxAeHUpWKf6iwlkasdjf'lkajWNZRTtjbJ4X0PIjkIQOCcLcimOJe4Y0";
         # For Encrypted setup with sops use the following:
-initialHashedPassword = "$6$yR/eBn9n/iJZ8x0O$LvUcT7JAVjr/OIBAOJO5.mlZ6.Edz/YBrw2YqYE5ly77qKfWBAjr/KX1rqU2wCnM5vkoqy46lXqyVkeU4Tp39/";
-       # hashedPasswordFile = config.sops.secrets.password_hash.path;
+        hashedPasswordFile = config.sops.secrets.password_hash.path;
 
         extraGroups = [
           "networkmanager"
