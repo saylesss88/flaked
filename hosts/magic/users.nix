c37878de55a714eea14283b7f9da84840d471d46
saylesss88 <saylesss87@proton.me>
2025-06-25 11:08:27 -0400

    sops

--- hosts/magic/users.nix
+++ hosts/magic/users.nix
@@ -18,8 +18,11 @@
         # Change me!
         description = "saylesss88";
         # Change me! generate with `mkpasswd -m SHA-512 -s`
-        # initialHashedPassword = "$6$knQSQp4le3uiy..3$gAUAugTxAeHUpWKf6iwo.omaLvm0j5xBXdzhmfFpBQKQRfmioQ1dDbjWNZRTtjbJ4X0PIjkIQOCcLcimOJe4Y0";
-        hashedPasswordFile = config.sops.secrets.password_hash.path;
+        # initialHashedPassword = "$6$knQSQp4le3uiy..3$gAUAugTxAeHUpWKf6iwlkasdjf'lkajWNZRTtjbJ4X0PIjkIQOCcLcimOJe4Y0";
+        # For Encrypted setup with sops use the following:
+initialHashedPassword = "$6$yR/eBn9n/iJZ8x0O$LvUcT7JAVjr/OIBAOJO5.mlZ6.Edz/YBrw2YqYE5ly77qKfWBAjr/KX1rqU2wCnM5vkoqy46lXqyVkeU4Tp39/";
+       # hashedPasswordFile = config.sops.secrets.password_hash.path;
+
         extraGroups = [
           "networkmanager"
           "wheel"
@@ -30,7 +33,7 @@
           "jr"
           "sudo"
         ];
-        shell = pkgs.nushell;
+        shell = pkgs.zsh;
         ignoreShellProgramCheck = true;
         packages = [
           pkgs.tealdeer
