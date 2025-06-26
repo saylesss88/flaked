20349bdb3f8657036dc1ec8c015cb856c9f1604b
saylesss88 <saylesss87@proton.me>
2025-06-26 06:36:30 -0400

    firefox

--- nixos/firefox.nix
+++ nixos/firefox.nix
@@ -12,9 +12,9 @@
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "sidebar.verticalTabs" = true;
       };
-      userChrome = builtins.readFile ../../.mozilla/firefox/8mab0vsl.default/chrome/MiniSthetic_Firefox/userChrome.css;
+      userChrome = builtins.readFile ./userChrome.css;
 
-      userContent = builtins.readFile ../../.mozilla/firefox/8mab0vsl.default/chrome/MiniSthetic_Firefox/userContent.css; # If you have userContent.css
+      userContent = builtins.readFile ./userContent.css; # If you have userContent.css
     };
   };
 }
