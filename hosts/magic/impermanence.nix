711b9674809a48d8f77eb15b75018b98b4a72d96
saylesss88 <saylesss87@proton.me>
2025-06-26 08:24:41 -0400

    impermanence script update

--- hosts/magic/impermanence.nix
+++ hosts/magic/impermanence.nix
@@ -5,10 +5,22 @@
 }: {
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     echo "Rollback running" > /mnt/rollback.log
-    mkdir /mnt
-    mount -t btrfs /dev/mapper/cryptroot /mnt
-    btrfs subvolume delete /mnt/root
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+     mkdir -p /mnt
+     mount -t btrfs /dev/mapper/cryptroot /mnt
+
+     # Recursively delete all nested subvolumes inside /mnt/root
+     btrfs subvolume list -o /mnt/root | cut -f9 -d' ' | while read subvolume; do
+       echo "Deleting /$subvolume subvolume..." >> /mnt/rollback.log
+       btrfs subvolume delete "/mnt/$subvolume"
+     done
+
+     echo "Deleting /root subvolume..." >> /mnt/rollback.log
+     btrfs subvolume delete /mnt/root
+
+     echo "Restoring blank /root subvolume..." >> /mnt/rollback.log
+     btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+     umount /mnt
   '';
 
   environment.persistence."/persist" = {
